ae85bf8c8fab5b650742ece01320903840ffc60c
ClaytonBranstetter <sky3_w0lf@hotmail.com>
2021-09-21 20:08:01 -0500

    exercise02

---
+++ lab01-exercise2.py
@@ -0,0 +1,14 @@
+'''
+Author: Clayton Branstetter
+KUID: 3089206
+Date: 09/21/2021
+Lab: lab#01
+Last modified: 09/21/2021
+Purpose: Introducing myself in code
+'''
+
+
+def user(name, major):
+  print("My name is {}\nI major in {}\nMy hobbies are\n\tCoding\n\tAnime\n\tCooking\n\tLeague of Legends".format(name, major))
+user("Clayton Branstetter", "Computer Science")
+
